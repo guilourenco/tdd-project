f286d1cbc802df162664dc11c7ba5252acbad6f0
guilourenco <guilhermelourencoj@gmail.com>
2021-03-03 21:06:40 -0300

    Validando os erros na view

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from lists.models import Item, List
-from django.utils.html import escape
 
 class HomePageTest(TestCase):
 
@@ -17,7 +16,34 @@ class HomePageTest(TestCase):
         self.assertIn('itemey 1', response.content.decode())
         self.assertIn('itemey 2', response.content.decode())
 
+class ListAndItemModelsTest(TestCase):
 
+    def test_saving_and_retrieving_items(self):
+        my_list = List()
+        my_list.save()
+
+        first_item = Item()
+        first_item.text = 'O primeiro item'
+        first_item.list = my_list
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'O segundo item'
+        second_item.list = my_list
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, my_list)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'O primeiro item')
+        self.assertEqual(first_saved_item.list, my_list)
+        self.assertEqual(second_saved_item.text, 'O segundo item')
+        self.assertEqual(second_saved_item.list, my_list)
 
 class ListViewTest(TestCase):
 
@@ -74,15 +100,3 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
-
-class NewListTest(TestCase):
-
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
-        response = self.client.post('/lists/new', data={'item_text': ''})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
